9df5cc0cadada26f97eb19ed30232b3b167d825a
tobi <tobibags19@gmail.com>
2023-05-11 09:21:58 -0500

    Create canvas object from user input

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from canvas import Canvas
 from shapes import Rectangle, Square
 
-canvas = Canvas(height=20, width=30, color=(255, 255, 255))
-r1 = Rectangle(x=1, y=6, height=7, width=10, color=(100, 200, 125))
-r1.draw(canvas)
-s1 = Square(x=1, y=3, side=3, color=(0, 100, 222))
-s1.draw(canvas)
-canvas.make('canvas.png')
+# Get canvas width and height from the user
+canvas_width = int(input("Enter canvas width: "))
+canvas_height = int(input("Enter canvas height: "))
+
+# Create a dictionary of color codes and prompt for color
+colors = {"white": (255, 255, 255), "black": (0, 0, 0)}
+canvas_color = input("Enter canvas color (white or black): ")
+
+# Create the canvas using provided data
+canvas = Canvas(height=canvas_height, width=canvas_width, color=colors[canvas_color])
+
+while True:
+
+    r1.draw(canvas)
+    s1.draw(canvas)
+    canvas.make('canvas.png')
